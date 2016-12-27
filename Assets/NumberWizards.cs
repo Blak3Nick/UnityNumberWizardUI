3db9c84c36bfa92ae3f2523660cc1200b8f80d6a
Blak3Nick <blaken2021@gmail.com>
2016-12-27 14:38:44 -0500

    changed some scenes

---
+++ Assets/NumberWizards.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 15;
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		guess = 500;		 		
+	}
+	void NextGuess() {
+		guess = Random.Range (min, max+1);	
+		text.text = guess.ToString();
+		maxGuessesAllowed --;
+		if (maxGuessesAllowed <=0) {
+		Application.LoadLevel("Lose");
+		}	
+	}
+
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+}
